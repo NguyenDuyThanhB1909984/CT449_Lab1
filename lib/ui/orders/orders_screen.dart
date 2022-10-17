2afa0f823069989a132a6ec07253233d2ab31283
Duy_Thanh <thanh08052001@gmail.com>
2022-10-17 20:49:22 +0700

    Quan ly trang thai voi provider - OrdersManager

--- lib/ui/orders/orders_screen.dart
+++ lib/ui/orders/orders_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'order_item_card.dart';
 import 'order_manager.dart';
 import '../shared/app_drawer.dart';
+import 'package:provider/provider.dart';
 
 class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
@@ -17,9 +18,13 @@ class OrdersScreen extends StatelessWidget {
         title: const Text('Your Orders'),
       ),
       drawer: const AppDrawer(),
-      body: ListView.builder(
-        itemCount: ordersManager.orderCount,
-        itemBuilder: (ctx, i) => OrderItemCard(ordersManager.orders[i]),
+      body: Consumer<OrdersManager>(
+        builder: (ctx, ordersManager, child) {
+          return ListView.builder(
+            itemCount: ordersManager.orderCount,
+            itemBuilder: (ctx, i) => OrderItemCard(ordersManager.orders[i]),
+          );
+        },
       ),
     );
   }
